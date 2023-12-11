49415577967248b667f0006a581d196eb8857e7b
Sameem-Amjad <sameemamjadarsu@gmail.com>
2023-12-11 13:18:31 +0500

    icons in bottom bar

--- navigators/bottomTab.js
+++ navigators/bottomTab.js
@@ -3,12 +3,34 @@ import Home from '../screens/home';
 import Login from '../screens/login';
 
 const Tab = createMaterialBottomTabNavigator();
-
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 function BottomTab() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={Home} />
-      <Tab.Screen name="Login" component={Login} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          tabBarLabel: 'Home',
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons name="home" color={color} size={26} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Login"
+        component={Login}
+        options={{
+          tabBarLabel: 'Login',
+          tabBarIcon: ({color}) => (
+            <MaterialCommunityIcons
+              name="account-lock-outline"
+              color={color}
+              size={26}
+            />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
